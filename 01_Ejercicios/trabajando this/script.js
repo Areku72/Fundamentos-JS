10242b1d77c73ae28d825b8042d4b5c7c64af08f
Areku72 <aronroa@liceovvh.cl>
2024-09-26 11:51:57 -0300

    ok

---
+++ 01_Ejercicios/trabajando this/script.js
@@ -0,0 +1,12 @@
+function 
+
+
+
+
+function login(element){
+    if (element.innerText == "Login"){
+        element.innerText == "Login"
+    }else {
+        element.innerText == "Login";
+    }
+}
